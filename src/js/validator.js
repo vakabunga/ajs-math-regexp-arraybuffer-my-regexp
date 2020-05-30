027739160360f3e243151c7b1f16fd674ef407e0
Vakabunga <shesterov@gmail.com>
2020-05-31 00:42:12 +0300

    class Validator is done; TODO tests

---
+++ src/js/validator.js
@@ -0,0 +1,8 @@
+export default class Validator {
+  static validateUsername(value) {
+    if (value.search(/^[a-z]+[a-z0-9-_]+[a-z]$/i) !== -1 && value.search(/\d{3,}/) === -1) {
+      return value;
+    }
+    throw new Error('invalid name');
+  }
+}
